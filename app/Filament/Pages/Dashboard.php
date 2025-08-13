3ccefbb76d8a639b401efba508caaa1c2d09d9a8
Crayzz2 <thiago.lacerda.fazzolo@hotmail.com>
2025-08-12 23:44:25 -0300

    Fixed Dashboard

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Pages;
+
+class Dashboard extends \Filament\Pages\Dashboard
+{
+    public function getHeaderActions(): array
+    {
+        return [
+            \Filament\Actions\Action::make('print')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->label(__('Summary'))
+                ->url('/print')
+                ->hidden(fn()=>!auth()->user()->hasAnyRole(['Super Admin', 'Painel de Controle'])),
+        ];
+    }
+}
